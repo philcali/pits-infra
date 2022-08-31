51b31c8c140341217b4f029e6841dd51f877b4f9
Philip Cali <philip.cali@gmail.com>
2022-08-31 19:02:08 -0400

    fix: github build

--- test/pits-infra.test.ts
+++ test/pits-infra.test.ts
@@ -7,15 +7,7 @@ test('Stack Creates Resources', () => {
     const stack = new PitsInfra.PitsInfraStack(app, 'MyTestStack');
     const template = Template.fromStack(stack);
 
-    template.hasResourceProperties('AWS::S3::Bucket', {
-        BucketName: 'philcali-pinthesky-storage'
-    });
-
     template.hasResourceProperties('AWS::IAM::Role', {
         RoleName: 'PinTheSkyRole'
     });
-
-    template.hasResourceProperties('AWS::IAM::ManagedPolicy', {
-        ManagedPolicyName: 'philcali-pinthesky-storage-policy',
-    })
 });
