c53566d6c118d02044d9272e7f1d8c19751463c1
Philip Cali <philip.cali@gmail.com>
2023-05-29 10:56:26 -0400

    chore: split up the infra

--- test/pits-infra.test.ts
+++ test/pits-infra.test.ts
@@ -4,10 +4,16 @@ import * as PitsInfra from '../lib/pits-infra-stack';
 
 test('Stack Creates Resources', () => {
     const app = new cdk.App();
-    const stack = new PitsInfra.PitsInfraStack(app, 'MyTestStack');
+    const stack = new PitsInfra.PitsDeviceConnectionStack(app, 'MyTestStack', {
+        bucketName: 'farts-mcgeehee'
+    });
     const template = Template.fromStack(stack);
 
     template.hasResourceProperties('AWS::IAM::Role', {
         RoleName: 'PinTheSkyRole'
     });
+
+    template.hasResourceProperties('AWS::S3::Bucket', {
+        BucketName: 'farts-mcgeehee'
+    })
 });
