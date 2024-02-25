4a19cbd518f72ae2dba4cceb6ee2d61cc5709583
Philip Cali <philip.cali@gmail.com>
2024-02-25 15:54:59 -0500

    fix: unsuet bash variable bucket name causing issues

--- lib/device/image/function.sh
+++ lib/device/image/function.sh
@@ -13,6 +13,7 @@ function handler() {
     CAMERA_NAME=$(basename "$(dirname "$video_object")")
     VIDEO_FILE=$(basename "$video_object")
     VIDEO_PATH=$(basename "$(dirname "$(dirname "$video_object")")")
+    BUCKET_NAME=$(echo "$video_object" | sed -E 's|^s3://([^/]+)/.+|\1|')
     TEMP_DIR=$(mktemp -d)
     TRIGGER=$(aws s3api head-object \
       --bucket "$BUCKET_NAME" \
@@ -23,7 +24,6 @@ function handler() {
     NEW_VIDEO_FILE="${VIDEO_FILE%.*}.${CONVERSION_FORMAT}"
     ffmpeg -r "$FRAMERATE" -i "$TEMP_DIR/$VIDEO_FILE" -c copy "$TEMP_DIR/$NEW_VIDEO_FILE"
     DURATION=$(mediainfo --Output=JSON "$TEMP_DIR/$NEW_VIDEO_FILE" | jq '.media.track[] | select(.["@type"] == "General") | .Duration' | tr -d '"')
-    BUCKET_NAME=$(echo "$video_object" | sed -E 's|^s3://([^/]+)/.+|\1|')
     aws s3 cp \
         "$TEMP_DIR/$NEW_VIDEO_FILE" \
         "s3://$BUCKET_NAME/$CONVERSION_PATH/$CAMERA_NAME/$NEW_VIDEO_FILE" \
